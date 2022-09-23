5577dc933ab2b27881cf9dfa5d47188ceb4896df
Michael Slavitsky <Slavitsky321@gmail.com>
2022-09-23 10:34:02 +0300

    Added the patrol script

--- Assets/Our Stuff/Scripts/Patrol.cs
+++ Assets/Our Stuff/Scripts/Patrol.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Patrol : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float speed;
+    private          float waitTime;
+    [SerializeField] float startWaitTime;
+
+    [SerializeField] Transform moveSpot;
+    [SerializeField] float     minX;
+    [SerializeField] float     maxX;
+    [SerializeField] float     minY;
+    [SerializeField] float     maxY;
+
+    private void Start()
     {
-        
+        waitTime = startWaitTime;
+        moveSpot.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        transform.position = Vector2.MoveTowards(transform.position, moveSpot.position, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, moveSpot.position) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                moveSpot.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+                waitTime = startWaitTime;
+            }
+
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
     }
 }
