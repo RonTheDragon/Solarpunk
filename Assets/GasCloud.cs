59f980db5a9ca6f6f906b61a87926fad9b6c0899
RonTheDragon <gweta.ron@gmail.com>
2022-09-23 16:43:39 +0300

    gas cloud

---
+++ Assets/GasCloud.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GasCloud : MonoBehaviour
+{
+    public int MaxHp;
+    int Hp;
+
+    public GameObject TheCloud;
+
+    public float RespawnCooldown;
+
+    float respawnTimeLeft;
+
+    Vector2 StartingScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Hp = MaxHp;
+        StartingScale = TheCloud.transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Hp < 0) { TheCloud.SetActive(false); respawnTimeLeft = RespawnCooldown;  Hp = MaxHp; }
+        if (respawnTimeLeft > 0) { respawnTimeLeft -= Time.deltaTime; } else if (TheCloud.activeSelf==false)
+        {
+            TheCloud.SetActive(true);
+            TheCloud.transform.localScale = StartingScale;
+        }
+    }
+
+    public void Sucked()
+    {
+        Hp--;
+        TheCloud.transform.localScale -= TheCloud.transform.localScale * 0.1f;
+    }
+}
--- Assets/ParticleCollision.cs
+++ Assets/ParticleCollision.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class ParticleCollision : MonoBehaviour
 {
+    public enum CollisionEffects { Water,Suck}
+    public CollisionEffects effect = CollisionEffects.Water;
     private void OnParticleCollision(GameObject other)
     {
-        if (other.tag == "Plant")
+        if (effect == CollisionEffects.Water)
         {
-            other.GetComponent<Plant>().GetWatered();
+            if (other.tag == "Plant")
+            {
+                other.GetComponent<Plant>().GetWatered();
+            }
+        }
+        else if (effect == CollisionEffects.Suck)
+        {
+            if (other.tag == "Enemy")
+            {
+                other.transform.parent.GetComponent<GasCloud>().Sucked();
+            }
+
         }
     }
 }
